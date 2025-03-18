1b800ae809e5a657f1953c64273a35594b7ff88a
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2025-03-18 11:06:21 -0300

    Add files via upload

--- teste2/Carro.gd
+++ teste2/Carro.gd
@@ -1,18 +1,15 @@
 extends CharacterBody2D
 
-@export var velocidade: float=200.0
-@export var velocidade_giro: float=200.0
+@export var velocidade: float = 200.0
+@export var velocidade_lateral: float = 150.0
 
-func _process(delta):
-	var direcao=0
-	
-	if Input.is_action_pressed("ui_left"): 
-		direcao=-1
+func _physics_process(delta):
+	var movimento = Vector2()
+
+	if Input.is_action_pressed("ui_left"):
+		movimento.x = -velocidade_lateral
 	elif Input.is_action_pressed("ui_right"):
-		direcao=1
-	rotation+=direcao*velocidade_giro*delta
+		movimento.x = velocidade_lateral
 
-func _physis_process(delta):
-	var direcao_velocidade = Vector2(0,-1).rotated(rotation)
-	velocity=direcao_velocidade*velocidade
+	velocity = movimento
 	move_and_slide()
--- teste2/Main.gd
+++ teste2/Main.gd
@@ -6,10 +6,17 @@ var tempo_contagem=3
 func _ready():
 	contador_label.text=str(tempo_contagem)
 	var contador_tempo=get_tree().create_timer(1.0)
-	
-contador_tempo.timeout.connect(_quando_tempo_acabar)
+	contador_tempo.timeout.connect(_quando_tempo_acabar)
 
 func _quando_tempo_acabar():
 	tempo_contagem-=1
+	contador_label.text=str(tempo_contagem)
 	
+	if tempo_contagem>0:
+		var novo_timer=get_tree().create_timer(1.0)
+		novo_timer.timeout.connect(_quando_tempo_acabar)
+	else:
+		iniciar_corrida()
 	
+func iniciar_corrida():
+	print("VAI")
